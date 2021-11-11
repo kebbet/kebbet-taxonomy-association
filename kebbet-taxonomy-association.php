5d7414cf146fa31905268d220220435cb6a3eaf9
Erik <11491369+kebbet@users.noreply.github.com>
2021-11-11 20:20:00 +0100

    Chnage visibility in menu UI.

--- kebbet-taxonomy-association.php
+++ kebbet-taxonomy-association.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Kebbet plugins - custom taxonomy: association
  * Plugin URI:        https://github.com/kebbet/kebbet-taxonomy-association
  * Description:       Register the custom taxonomy association
- * Version:           20210806.01
+ * Version:           20211111.01
  * Author:            Erik Betshammar
  * Author URI:        https://verkan.se
  * Requires at least: 5.7
@@ -89,11 +89,10 @@ function register() {
 		'public'                => false,
 		'show_ui'               => true,
 		'show_admin_column'     => true,
-		'show_in_nav_menus'     => true,
 		'update_count_callback' => '_update_post_term_count',
 		'query_var'             => false,
 		'show_in_rest'          => true,
-		'rewrite'               => array(),
+		'rewrite'               => false,
 		'description'           => __( 'Association tags.', 'kebbet-taxonomy-association' ),
 	);
 
